8dc6fdb3843946a09a94d66629b9c7cb437af566
Wes Duff <slysop@gmail.com>
2022-11-02 08:21:02 -0600

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -5,11 +5,11 @@ function myNewFunction(endpoint='none', type='none'){
 let test1 = new myNewFunction('yahoo.com', 'yahoo');
 
 test1 instanceof myNewFunction
-
+//
 let test2 = myNewFunction()
 test2 instanceof myNewFunction
 console.log(test2)
-
+//
 let test3 = new myNewFunction()
 test3 instanceof myNewFunction
 console.log(test3)
