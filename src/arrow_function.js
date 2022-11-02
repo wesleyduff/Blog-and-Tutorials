5f44ba978edbf97fb29d34993f263884da72521e
Wes Duff <slysop@gmail.com>
2022-11-02 07:02:20 -0600

    Update arrow_function.js

--- src/arrow_function.js
+++ src/arrow_function.js
@@ -3,6 +3,10 @@ const mappedCollection = collection.map(item => item + " : addition");
 //['a : addition', 'b : addition', 'c : addition']
 
 //
+var root_config = {
+ endpoint: "charter.com"
+}
+
 function test(){
     const config = { 
         endpoint: "google.com"
@@ -14,5 +18,16 @@ function test(){
       getUrl:  function(){ return this.endpoint }
     }
   
+    return {options}
+}
+
+function test2(){
+    
+    const options = {
+      url: (() => this.root_config.endpoint)(),
+      endpoint : 'yahoo.com',
+      getUrl:  function(){ return this.endpoint }
+    }
+  
     return options
-  }
+}
